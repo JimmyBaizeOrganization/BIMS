07668fab1dbf7dc5450e3ed671728b5a8ace5fa6
cbw2008cbw <283304725@qq.com>
2015-01-01 21:10:58 +0800

    11

--- BIMS/DeFrmMain.cs
+++ BIMS/DeFrmMain.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using Tools;
 
 namespace BIMS
 {
@@ -25,7 +26,7 @@ namespace BIMS
             mtableLayoutPanel.Location = new Point(0, 0);
             mtableLayoutPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
 
-            for (int i = 0; i < 16; i++) 
+            for (int i = 0; i < 7; i++) 
             {
                 Panel pan = new Panel();
                 pan.Name = "Panel" + i.ToString();
@@ -42,13 +43,25 @@ namespace BIMS
                 pic.Location = new Point(38, 29);
                 pic.BackColor = Color.LightBlue;
                 pic.Parent =pan;
-
             }
 
-            //mpanel.Location = new Point(cLBC_left.Width, pictureBox1.Height);
-            //Console.WriteLine (this .Size);
-            //mpanel.Size = new Size(this.Size.Width - cLBC_left.Width-18,this.Size.Height - pictureBox1 .Size.Height-48);
-            //Console.WriteLine(mpanel.Size);
+            Button_Close.Parent = pictureBox1;
+            Button_Document .Parent = pictureBox1;
+            Button_Help .Parent = pictureBox1;
+            Button_Home .Parent = pictureBox1;
+            Button_Locked .Parent = pictureBox1;
+            Button_Setting .Parent = pictureBox1;
+            Button_Sum .Parent = pictureBox1;
+
+            ReflectTools rt = new ReflectTools("BIMS", "BIMS", "DED194E_9S1YK2K2");
+            DED194E_9S1YK2K2 aa = (DED194E_9S1YK2K2)rt.MObj;
+
+            aa.Location = new Point(200, 200);
+            aa.BackColor = Color.Black;
+            aa.Size = new Size(10, 10);
+            this.Controls.Add(aa);
+            aa.Parent = this.Controls.Find("PictureBox0",true)[0];
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -66,9 +79,21 @@ namespace BIMS
 
         }
 
-        private void DeFrmMain_Resize(object sender, EventArgs e)
+        private void Button_Close_Click(object sender, EventArgs e)
         {
-            //mpanel.Size = new Size(this.Size.Width - cLBC_left.Width - 18, this.Size.Height - pictureBox1.Size.Height - 48);
+            this.Close();
         }
+
+        private void Button_Help_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(this.Size.ToString());
+            Console.WriteLine(this.Location.ToString());
+        }
+
+        private void Button_Home_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
