a58e2fdae815d3ee9be6d3b049eb413fdea41ee4
cbw2008cbw <283304725@qq.com>
2015-01-01 16:40:28 +0800

    2333333333

--- BIMS/DeFrmMain.cs
+++ BIMS/DeFrmMain.cs
@@ -25,7 +25,7 @@ namespace BIMS
             mtableLayoutPanel.Location = new Point(0, 0);
             mtableLayoutPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
 
-            for (int i = 0; i < 16; i++) 
+            for (int i = 0; i < 7; i++) 
             {
                 Panel pan = new Panel();
                 pan.Name = "Panel" + i.ToString();
@@ -42,13 +42,17 @@ namespace BIMS
                 pic.Location = new Point(38, 29);
                 pic.BackColor = Color.LightBlue;
                 pic.Parent =pan;
-
             }
 
-            //mpanel.Location = new Point(cLBC_left.Width, pictureBox1.Height);
-            //Console.WriteLine (this .Size);
-            //mpanel.Size = new Size(this.Size.Width - cLBC_left.Width-18,this.Size.Height - pictureBox1 .Size.Height-48);
-            //Console.WriteLine(mpanel.Size);
+            Button_Close.Parent = pictureBox1;
+            Button_Document .Parent = pictureBox1;
+            Button_Help .Parent = pictureBox1;
+            Button_Home .Parent = pictureBox1;
+            Button_Locked .Parent = pictureBox1;
+            Button_Setting .Parent = pictureBox1;
+            Button_Sum .Parent = pictureBox1;
+
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -66,9 +70,10 @@ namespace BIMS
 
         }
 
-        private void DeFrmMain_Resize(object sender, EventArgs e)
+        private void Button_Close_Click(object sender, EventArgs e)
         {
-            //mpanel.Size = new Size(this.Size.Width - cLBC_left.Width - 18, this.Size.Height - pictureBox1.Size.Height - 48);
+            this.Close();
         }
+
     }
 }
---
+++ BIMS/Device.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows .Forms;
+
+namespace BIMS
+{
+    class BaseDevice: PictureBox 
+    {
+        private Form mform;
+
+        public Form Mform
+        {
+            get { return mform; }
+            set { mform = value; }
+        }
+        public BaseDevice()
+        {
+            this.MouseEnter += new EventHandler(Common_MouseEnter);
+        }
+          private void Common_MouseEnter(object sender, EventArgs e)
+        {
+            if (Mform == null || Mform.IsDisposed) 
+            { 
+            newform() ;
+            }
+            //Mform.Location =new PointToClient()
+            Mform.Show();
+            Mform.Focus();
+        }
+          public virtual void newform() { }
+    }
+
+    class DED194E_9S1YK2K2 : BaseDevice
+    { 
+
+        public DED194E_9S1YK2K2()
+        {
+           
+        }
+      
+    }
+}
